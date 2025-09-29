c95c28f7aa8d8033f5e75e92177250c3b9a1af6d
Blueapple031 <61594585+Blueapple031@users.noreply.github.com>
2025-09-29 11:22:43 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/masil/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package masil.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
