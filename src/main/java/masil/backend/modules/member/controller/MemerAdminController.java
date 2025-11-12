f278fe728c1ed88683e8d9202dbaf70cc247d11d
김태윤 <77539625+pykido@users.noreply.github.com>
2025-11-13 00:58:18 +0900

    Feature/#14 관리자페이지 UI 및 상태 조회 페이지 개발 (#26)
    * feat : 멤버 상태값 구상완료!
    
    * feat : 관리자페이지 UI 초안 설계
    
    * feat : 충돌 해결

---
+++ src/main/java/masil/backend/modules/member/controller/MemerAdminController.java
@@ -0,0 +1,11 @@
+package masil.backend.modules.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/members")
+public class MemerAdminController {
+}
