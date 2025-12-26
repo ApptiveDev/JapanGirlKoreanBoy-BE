25c8e502b67b9b964524910d5bdeb00bceebc832
pykido <animoto1@naver.com>
2025-12-26 18:20:03 +0900

    feat : 의존성 및 config 추가

---
+++ src/main/java/masil/backend/global/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package masil.backend.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/firebase-service-account.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
